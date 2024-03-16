f70d0ab3b56d304f95d2789f31b48c9a9da5ab1b
Umar Qazi <77536082+umarqazii@users.noreply.github.com>
2024-03-16 22:08:16 +0500

    Update App.js
    Added live price of cardano

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   // Define the state to store the latest price
   const [btcprice, setbtcPrice] = useState(null);
   const [ethprice, setethPrice] = useState(null);
+  const [adaPrice, setadaPrice] = useState(null);
   
   useEffect(() => {
     const btc = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
     const eth = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade');
+    const ada = new WebSocket('wss://stream.binance.com:9443/ws/adausdt@trade');
 
     btc.onmessage = (event) => {
       // Extract the price from the incoming message and update the state
@@ -22,10 +24,17 @@ function App() {
       setethPrice(data.p);
     }
 
+    ada.onmessage = (event) => {
+      // Extract the price from the incoming message and update the state
+      const data = JSON.parse(event.data);
+      setadaPrice(data.p);
+    }
+
     // Cleanup function
     return () => {
       btc.close();
       eth.close();
+      ada.close();
     };
   }, []);
 
@@ -33,8 +42,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Realtime Prices</h1>
-        <span>Bitcoin:<p>{btcprice !== null ? `$${btcprice}` : 'Loading...'}</p></span>
-        <span>Ethereum:</span><p>{ethprice !== null ? `$${ethprice}` : 'Loading...'}</p>
+        <p>Bitcoin: {btcprice !== null ? `$${btcprice}` : 'Loading...'}</p>
+        <p>Ethereum: {ethprice !== null ? `$${ethprice}` : 'Loading...'}</p>
+        <p>Cardano: {adaPrice !== null ? `$${adaPrice}` : 'Loading...'}</p>
       </header>
     </div>
   );
